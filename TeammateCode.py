02da31c58d2f5d6e6b528fda45d169d8a45119b8
stibycap <158280320+stibycap@users.noreply.github.com>
2024-02-17 01:14:08 +0900

    Update TeammateCode.py

--- TeammateCode.py
+++ TeammateCode.py
@@ -1,20 +1,56 @@
-import pygame
-import sys
+from tkinter import *
+from random import *
+
+#сделаем список карт
+cards = [6, 8, 9, 10, 'валет', 'дама', 'король', 'туз'] * 4
+shuffle(cards)
+cmd = 0
 
 
-class Code:
-    def __init__(self):
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("")
-        self.clock = pygame.time.Clock()
+def take():
+    global cmd, cards
+    card = cards.pop()
+    if card == 'валет':
+        card = 2
+    if card == 'дама':
+        card = 3
+    if card == 'король':
+        card = 4
+    if card == 'туз':
+        card = 11
+    cmd += card
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+    if cmd > 21:
+        lbl3['text'] = 'вы проиграли, так как ' + str(cmd) + ' очков'
+        lbl2["text"] = 'вы проиграли'
+    else:
+        lbl2["text"] = "у вас " + str(cmd) + " очков"
 
-            self.screen.fill((0, 0, 255))
-            pygame.display.flip()
-            self.clock.tick(30)
+
+def enough():
+    global cmd
+    if cmd == 21:
+        lbl3['text'] = 'поздравляем, у вас ровно 21'
+    if cmd < 21:
+        lbl3['text'] = 'у вас ровно' + str(cmd) + ' очков'
+
+#обозначим
+tk = Tk()
+tk.geometry('300x140')
+
+lbl1 = Label(tk, text="Игра в Black-Jack", fg="black")
+lbl1.place(x=100, y=0)
+
+lbl2 = Label(tk, text="У вас 0 очков", fg="black")
+lbl2.place(x=110, y=30)
+
+btn = Button(tk, width="15", text="Взять", command=take)
+btn.place(x=20, y=60)
+
+btn2 = Button(tk, width="15", text="Стоп", command=enough)
+btn2.place(x=160, y=60)
+
+lbl3 = Label(tk, text="Результат игры", fg="red")
+lbl3.place(x=90, y=100)
+
+tk.mainloop()
